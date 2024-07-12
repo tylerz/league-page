9882c60bde553160c1526198dfbbe8afe66fb830
tylerz <tyzimmer@gmail.com>
2024-07-11 20:42:36 -0600

    removefooter

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -97,38 +97,38 @@
 	}
 </style>
 
-<div class="footerSpacer" style="height: {footerHeight}px;" />
+// <div class="footerSpacer" style="height: {footerHeight}px;" />
 
-<!-- footer with update notice -->
-<footer bind:this={el}>
-    {#if outOfDate}
-	    <p class="updateNotice">There is an update available for your League Page. <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iv-updates">Follow the Update Instructions</a> to get all of the newest features!</p>
-    {/if}
-    {#if managersOutOfDate}
-	    <p class="updateNotice">Your managers page needs an update, <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#2-add-managers">please follow the instructions</a> to get the most up-to-date experience.</p>
-    {/if}
-	<div id="navigation">
-		<ul>
-			{#each tabs as tab}
-				{#if !tab.nest}
-					<li><div class="navLink" on:click={() => goto(tab.dest)}>{tab.label}</div></li>
-				{:else}
-					{#each tab.children as child}
-                        <!-- Shouldn't show Managers tab unless managers has been populated -->
-				        {#if child.label != "Managers" || managers.length > 0}
-                            <li><div class="navLink" on:click={() => goto(child.dest)}>{child.label}</div></li>
-                        {/if}
-					{/each}
-				{/if}
-			{/each}
-		</ul>
-	</div>
-	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
-	<br />
-	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
-	<!-- You can remove the donation link (although any donations to help
-	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
-</footer>
+// <!-- footer with update notice -->
+// <footer bind:this={el}>
+    // {#if outOfDate}
+	    // <p class="updateNotice">There is an update available for your League Page. <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iv-updates">Follow the Update Instructions</a> to get all of the newest features!</p>
+    // {/if}
+    // {#if managersOutOfDate}
+	    // <p class="updateNotice">Your managers page needs an update, <a href="https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#2-add-managers">please follow the instructions</a> to get the most up-to-date experience.</p>
+    // {/if}
+	// <div id="navigation">
+		// <ul>
+			// {#each tabs as tab}
+				// {#if !tab.nest}
+					// <li><div class="navLink" on:click={() => goto(tab.dest)}>{tab.label}</div></li>
+				// {:else}
+					// {#each tab.children as child}
+                        // <!-- Shouldn't show Managers tab unless managers has been populated -->
+				        // {#if child.label != "Managers" || managers.length > 0}
+                            // <li><div class="navLink" on:click={() => goto(child.dest)}>{child.label}</div></li>
+                        // {/if}
+					// {/each}
+				// {/if}
+			// {/each}
+		// </ul>
+	// </div>
+	// <!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
+	// <span class="copyright">&copy; 2021 - {year} <a href="https://github.com/nmelhado/league-page">League Page</a></span>
+	// <br />
+	// <!-- PLEASE DO NOT REMOVE THE BUILT BY -->
+	// <span class="creator">Built by <a href="http://www.nmelhado.com/">Nicholas Melhado</a><br /></span>
+	// <!-- You can remove the donation link (although any donations to help
+	 // maintain and enhance League Page would be greatly appreciated!) -->
+	// Love League Page? Please consider <a href="https://www.buymeacoffee.com/nmelhado">donating</a> to support enhancements or just to say thank you!
+// </footer>
