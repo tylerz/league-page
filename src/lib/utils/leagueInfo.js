f42e07136af14e9a73e03327f64697f7919c1575
tylerz <tyzimmer@gmail.com>
2024-07-11 21:41:35 -0600

    philo

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -50,8 +50,8 @@ export const managers = [
       "favoritePlayer": 8676, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": null,
-      "tradingScale": 8, // 1 - 10
+      "philosophy": "diversify",
+      "tradingScale": 7, // 1 - 10
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
