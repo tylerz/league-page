7928ca7e744c521ad97bfbf483abbab9d6631080
tylerz <tyzimmer@gmail.com>
2025-02-06 07:53:42 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1048314283152527360"; // your league ID
+export const leagueID = "1180931001050927104" // 2024= "1048314283152527360"; // your league ID
 export const leagueName = "DUDES IN POOLS DYNASTY LEAGUE"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
